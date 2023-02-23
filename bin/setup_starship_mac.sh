314bcf5fe2dc53bca49e85287eafd75b4048dc02
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2023-02-23 21:09:25 +0900

    starship setup

---
+++ bin/setup_starship_mac.sh
@@ -0,0 +1,20 @@
+# !/bin/bash -eu
+
+# Install starship
+if !(type "starship" > /dev/null 2>&1); then
+    echo "# Install starship >>>>>>>"
+    brew install starship
+    (echo; echo 'eval "$(starship init zsh)"') >> ~/.zshrc
+fi
+echo "# starship version:"
+echo `starship --version`
+
+# Add .config/starship.toml
+echo
+read -n1 -p "# Add .config/starship.toml? (y/N)" yn
+if [[ $yn = [yY] ]]; then
+    echo "# Add .config/starship.toml >>>>>>>"
+    curl --create-dirs --output .config/starship.toml \
+        https://raw.githubusercontent.com/yuta0428/dotfiles/master/.config/starship_osx.toml
+fi
+echo
