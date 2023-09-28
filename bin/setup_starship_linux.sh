19a045954e278bd48374b911c36026bc7babaaaf
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2023-09-29 00:51:03 +0900

    rename mac -> linux

--- bin/setup_starship_linux.sh
+++ bin/setup_starship_linux.sh
@@ -4,7 +4,7 @@
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
 if !(type "starship" > /dev/null 2>&1); then
     echo "# Install starship >>>>>>>"
-    brew install starship
+    curl -sS https://starship.rs/install.sh | sh
     (echo; echo 'eval "$(starship init zsh)"') >> ~/.zshrc
 fi
 echo "# starship version:"
