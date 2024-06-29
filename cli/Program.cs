a2c5acda5890e44a02a0759dd16cd1be5d71fc2c
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2024-06-30 02:01:13 +0900

    add ConsoleAppFramework

---
+++ cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleAppFramework;
+
+ConsoleApp.Run(args, Commands.Hello);
+
+
+static class Commands
+{
+    /// <summary>
+    /// Display Hello.
+    /// </summary>
+    /// <param name="message">-m|-mm, Message to show.</param>
+    public static void Hello(string message) => Console.Write($"Hello, {message}");
+
+}
